173dc6a533e82ec97c0ab885de3fed96e8ea3289
achanta3215 <achanta3215@gmail.com>
2016-03-15 20:22:27 +0530

    Initial commit

---
+++ app/src/main/java/com/geniuscreations/successtaskanalyzer/MainActivity.java
@@ -0,0 +1,15 @@
+package com.geniuscreations.successtaskanalyzer;
+
+import android.app.Activity;
+
+/**
+ * Created by Krishna sameer on 3/15/2016.
+ */
+public class MainActivity extends Activity {
+
+
+
+
+
+
+}
